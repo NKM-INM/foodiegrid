4b310f2e3433f9818ab207dad409a1369251b0a2
NKM-INM <sid.maverick20xx@gmail.com>
2025-09-06 05:27:17 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,4 +1,6 @@
 // lib/supabase.ts
+
+
 import { createClient } from "@supabase/supabase-js";
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
@@ -13,8 +15,10 @@ console.log(
 );
 
 // Récup env vars
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const url  = process.env.NEXT_PUBLIC_SUPABASE_URL  || "";
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+
+export const supabase = createClient(url, anon);
 
 // Garde-fous clairs (évite les erreurs floues type "supabaseUrl is required")
 if (!url) throw new Error("Missing env: NEXT_PUBLIC_SUPABASE_URL");
